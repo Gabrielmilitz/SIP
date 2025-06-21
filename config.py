8284eeb380bab1e445a473edf07fed182da5e3c5
gabriel <gabrielmarinmilitz@gmail.com>
2025-06-21 14:38:09 -0300

    atualizando informacoesde seguranca

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SECRET_KEY = os.urandom(24)
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/sip_db'
+load_dotenv(os.path.join(basedir, '.env'))  # Carrega as variáveis do .env
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads')
 
@@ -11,6 +13,6 @@ UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads')
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = 'gabrielmarinmilitz@gmail.com'
-MAIL_PASSWORD = 'exyn ieen xiao bjjz '
-MAIL_DEFAULT_SENDER = 'gabrielmarinmilitz@gmail.com'
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
