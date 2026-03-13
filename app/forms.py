55f5286af0f5c42c6f02b8b4cbc0eb1e53e82016
Gabrielmilitz <gabrielmarinmilitz@gmail.com>
2026-03-13 20:42:42 -0300

    ajustes

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, TextAreaField, SubmitField, SelectField, PasswordField, DateField
 from wtforms.validators import DataRequired, Email
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import SelectField
+
+
 class ColaboradorForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
@@ -10,59 +11,145 @@ class ColaboradorForm(FlaskForm):
 
 
 class ProcessoForm(FlaskForm):
+
     colaborador = SelectField('Colaborador', coerce=int, validators=[DataRequired()])
+
     descricao = TextAreaField('Descrição do Processo', validators=[DataRequired()])
+
     status = SelectField(
         'Status',
-        choices=[('Conforme', 'Conforme'), ('Desvio', 'Desvio')],
+        choices=[
+            ('Conforme', 'Conforme'),
+            ('Desvio', 'Desvio')
+        ],
         validators=[DataRequired()]
     )
-    observacoes = TextAreaField('Observações')
-    imagem = FileField('Imagem', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Apenas imagens são permitidas')])
 
-    # Novos campos para a tabela DetalhesProcesso
-    coordenacao = StringField('Coordenação')
-    lideranca = StringField('Liderança')
+    observacoes = TextAreaField('Observações')
+
+    imagem = FileField(
+        'Imagem',
+        validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Apenas imagens são permitidas')]
+    )
+
+    coordenacao = SelectField(
+        'Coordenação',
+        choices=[
+            ('', 'Selecione'),
+            ('larissa.gregoria', 'larissa.gregoria'),
+            ('eluane.machado', 'eluane.machado'),
+            ('lilian.flores', 'lilian.flores')
+        ]
+    )
+
+    lideranca = SelectField(
+        'Liderança',
+        choices=[
+            ('', 'Selecione'),
+            ('gabriel.militz', 'gabriel.militz'),
+            ('tacio.pozzati', 'tacio.pozzati'),
+            ('diogo.machado', 'diogo.machado'),
+            ('tais.melo', 'tais.melo'),
+            ('ilto.rodrigues', 'ilto.rodrigues'),
+            ('nathalia.moraes', 'nathalia.moraes'),
+            ('mauricio.konig', 'mauricio.konig'),
+            ('kerolin.vargas', 'Kerolin.Vargas'),
+            ('gabriela.doring', 'gabriela.doring')
+        ]
+    )
+
     data_inspecao = DateField('Data da Inspeção', format='%Y-%m-%d')
-    inspetor = StringField('Inspetor')
+
+    inspetor = SelectField(
+        'Inspetor',
+        choices=[
+            ('', 'Selecione'),
+            ('andressa.castro', 'andressa.castro'),
+            ('andressa.belmiro', 'andressa.belmiro'),
+            ('andrieli.bock', 'andrieli.bock'),
+            ('emilie.vollmann', 'emilie.vollmann'),
+            ('fabiana.soares', 'fabiana.soares'),
+            ('erika.figueira', 'erika.figueira'),
+            ('gabrielle.pippi', 'gabrielle.pippi'),
+            ('guilherme.losekann', 'guilherme.losekann'),
+            ('marivane.fernandes', 'marivane.fernandes'),
+            ('monica.laucksen', 'monica.laucksen')
+        ]
+    )
+
     cpf_cnpj = StringField('CPF/CNPJ')
     coop = StringField('Coop')
-    tipo = SelectField('Tipo', choices=[
-    ('Cooperativa', 'Cooperativa'),
-    ('Autorizar', 'Autorizar'),
-    ('Autoatendimento', 'Autoatendimento')
-])
+
+    tipo = SelectField(
+        'Tipo',
+        choices=[
+            ('Cooperativa', 'Cooperativa'),
+            ('Autorizar', 'Autorizar'),
+            ('Autoatendimento', 'Autoatendimento')
+        ]
+    )
+
     data_execucao = DateField('Data de Execução', format='%Y-%m-%d')
-    interno_externo = SelectField('Interno ou Externo', choices=[('Interno', 'Interno'), ('Externo', 'Externo')])
-    tipo_autorizacao = SelectField('Tipo de Autorização', choices=[
-    ('BEM_NOVO', 'BEM_NOVO'),
-    ('CERTIDAO', 'CERTIDAO'),
-    ('ENDERECO', 'ENDERECO'),
-    ('FONTE_RENDA', 'FONTE_RENDA'),
-    ('PESSOA', 'PESSOA'),
-    ('PRODUTIVIDADE', 'PRODUTIVIDADE'),
-    ('PRODUTOR', 'PRODUTOR'),
-    ('RELACIONAMENTO', 'RELACIONAMENTO'),
-    ('TRIBUTACAO', 'TRIBUTACAO')])
-    tipo_erro = SelectField('Tipo de Erro', choices=[
-    ('Sem erro', 'Sem erro'),
-    ('Aprovacao Incorreta', 'Aprovação Incorreta'),
-    ('Preenchimento Incorreto', 'Preenchimento Incorreto'),
-    ('Reprovacao Incorreta', 'Reprovação Incorreta'),
-    ('Reprovacao Incompleta', 'Reprovação Incompleta'),
-    ('Documento Invalido', 'Documento Inválido')
-])
-    gravidade = SelectField('Gravidade', choices=[
-    ('Sem Erro', 'Sem Erro'),
-    ('Grave', 'Grave'),
-    ('Gravissimo', 'Gravíssimo')
-])
-    desvio_atencao = SelectField('Desvio de Atenção', choices=[('Sim', 'Sim'), ('Nao', 'Não')])
-    reversao = SelectField('Reversão', choices=[
-    ('Grave', 'Grave'),
-    ('Gravissimo', 'Gravíssimo'),
-    ('Nao', 'Não')
-])
+
+    interno_externo = SelectField(
+        'Interno ou Externo',
+        choices=[
+            ('Interno', 'Interno'),
+            ('Externo', 'Externo'),
+            ('Chamado', 'Chamado')
+        ]
+    )
+
+    tipo_autorizacao = SelectField(
+        'Tipo de Autorização',
+        choices=[
+            ('BEM_NOVO', 'BEM_NOVO'),
+            ('CERTIDAO', 'CERTIDAO'),
+            ('ENDERECO', 'ENDERECO'),
+            ('FONTE_RENDA', 'FONTE_RENDA'),
+            ('PESSOA', 'PESSOA'),
+            ('PRODUTIVIDADE', 'PRODUTIVIDADE'),
+            ('PRODUTOR', 'PRODUTOR'),
+            ('RELACIONAMENTO', 'RELACIONAMENTO'),
+            ('TRIBUTACAO', 'TRIBUTACAO')
+        ]
+    )
+
+    tipo_erro = SelectField(
+        'Tipo de Erro',
+        choices=[
+            ('Sem erro', 'Sem erro'),
+            ('Aprovacao Incorreta', 'Aprovação Incorreta'),
+            ('Preenchimento Incorreto', 'Preenchimento Incorreto'),
+            ('Reprovacao Incorreta', 'Reprovação Incorreta'),
+            ('Reprovacao Incompleta', 'Reprovação Incompleta'),
+            ('Documento Invalido', 'Documento Inválido')
+        ]
+    )
+
+    gravidade = SelectField(
+        'Gravidade',
+        choices=[
+            ('Sem Erro', 'Sem Erro'),
+            ('Grave', 'Grave'),
+            ('Gravissimo', 'Gravíssimo')
+        ]
+    )
+
+    desvio_atencao = SelectField(
+        'Desvio de Atenção',
+        choices=[('Sim', 'Sim'), ('Nao', 'Não')]
+    )
+
+    reversao = SelectField(
+        'Reversão',
+        choices=[
+            ('Grave', 'Grave'),
+            ('Gravissimo', 'Gravíssimo'),
+            ('Nao', 'Não')
+        ]
+    )
+
     fluxos = StringField('Quais Fluxos')
     solicitante = StringField('Solicitante')
 
@@ -70,7 +157,7 @@ class ProcessoForm(FlaskForm):
 
 
 class DeletarTodosForm(FlaskForm):
-    submit = SubmitField('🗑️ Deletar Todos os Registros')
+    submit = SubmitField('Deletar Todos os Registros')
 
 
 class LoginForm(FlaskForm):
@@ -78,18 +165,13 @@ class LoginForm(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired()])
     submit = SubmitField('Entrar')
 
-# segurança = precisa melhorar muito
+
 class TrocarSenhaForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha_atual = PasswordField('Senha Atual', validators=[DataRequired()])
     nova_senha = PasswordField('Nova Senha', validators=[DataRequired()])
     submit = SubmitField('Trocar Senha')
 
-
-
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import SubmitField
 
 class PerfilForm(FlaskForm):
     foto = FileField('Selecionar Foto', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Apenas imagens.')])
